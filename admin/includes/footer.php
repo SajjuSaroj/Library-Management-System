87febbc7d8cd1b0a3d16eb1da7ba9865d630186e
SajjuSaroj <sk5472221@gmail.com>
2021-11-12 10:48:48 +0530

    Initial commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                     Online Library Management System |<a href="https://www.facebook.com/saroj.sahu.351756" target="_blank"> Designed by : SAROJ KUMAR SAHU</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2019
+   Online Library Management System |<a href="https://www.facebook.com/saroj.sahu.351756" target="_blank"> Designed by : SAROJ KUMAR SAHU</a> </a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
